088a9465214823567297dd2fc798cf9c0300fa8a
Cayle <calebabhulimhen@gmail.com>
2022-10-09 00:47:05 -0500

    Boilerplate code for endpoints to handle baggage scanning

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('test', views.test, name = 'test'),
+    path('analyzeBagImage', views.analyzeImage, name='analyze-bag-image'),
+    path('passengerBagInfo', views.showCheckedBagsInfo, name='passenger-bag-info')
 ]
